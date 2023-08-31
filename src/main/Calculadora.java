6e9e629fd3a5799bbf4951a1c4d5f1ed3baee96f
Henrique Dani <henriquedani172004@gmail.com>
2023-08-31 11:20:07 -0300

    Implementando método subtracao

--- src/main/Calculadora.java
+++ src/main/Calculadora.java
@@ -6,6 +6,11 @@ public class Calculadora {
     public int adicao(int num1, int num2) {
         return num1 + num2;
     }
+    
+    public int subtracao(int num1, int num2) {
+    	return num1 - num2;
+    	
+    }
 
 }
 
